68a0e07e14d225c9941e4d3bb128dee18ec77b5e
harmolipi <nikob381@gmail.com>
2021-11-15 15:33:46 -0500

    Moved display to project object class

--- src/page-load.js
+++ src/page-load.js
@@ -1,15 +1,17 @@
 import { displayNewProjectButton } from './new-project-button-module';
-import { display } from './display-project-module';
+import { Project } from './project-module';
 import { TodoItem } from './todo-item-module';
 
 const pageLoad = () => {
   const myTodo = new TodoItem('test title', 'test description', 'test due data', 'test priority', 'Medium', true);
+  const myProject = new Project('Project title', 'Project description');
+  myProject.addTodo(myTodo);
   const myTodoDom = myTodo.displayTodoItem();
   const todoList = document.querySelector('.project-todos');
   const projects = document.querySelector('#projects');
   projects.prepend(displayNewProjectButton());
   todoList.appendChild(myTodoDom);
-  projects.appendChild(display('New Project', myTodo.displayTodoItem()));
+  projects.appendChild(myProject.displayProject());
 };
 
 export default pageLoad;
--- src/project-module.js
+++ src/project-module.js
@@ -3,4 +3,45 @@ class Project {
     this.name = name;
     this.todoItems = [];
   }
+
+  displayProject() {
+    const projectContainer = document.createElement('div');
+    projectContainer.classList.add('project-container', 'card', 'ma2', 'w5', 'h5');
+
+    const projectBody = document.createElement('div');
+    projectBody.classList.add('project-body', 'card-body', 'overflow-scroll');
+
+    const projectHeader = document.createElement('div');
+    projectHeader.classList.add('project-header', 'flex', 'flex-row-reverse', 'flex-wrap', 'justify-between');
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('button-container');
+
+    const editButton = document.createElement('button');
+    editButton.classList.add('edit-button', 'badge', 'warning', 'pa1');
+    editButton.innerText = 'Edit';
+
+    const projectTitle = document.createElement('h3');
+    projectTitle.classList.add('project-title', 'card-title');
+    projectTitle.innerText = this.name;
+
+    const projectTodos = document.createElement('div');
+    projectTodos.classList.add('project-todos', 'mt-4');
+
+    projectContainer.appendChild(projectBody);
+    projectBody.appendChild(projectHeader);
+    projectHeader.appendChild(buttonContainer);
+    buttonContainer.appendChild(editButton);
+    projectHeader.appendChild(projectTitle);
+    projectBody.appendChild(projectTodos);
+    projectTodos.appendChild(this.todoItems[0].displayTodoItem());
+
+    return projectContainer;
+  }
+
+  addTodo(todo) {
+    this.todoItems.push(todo);
+  }
 }
+
+export { Project };
