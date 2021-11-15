4190e5f10bdb8ea6e70cddf4de7704066de7c76f
harmolipi <nikob381@gmail.com>
2021-11-15 14:26:23 -0500

    Created display function for todo items

--- src/todo-item-module.js
+++ src/todo-item-module.js
@@ -6,4 +6,53 @@ class TodoItem {
     this.priority = priority;
     this.completed = completed;
   }
+
+  displayTodoItem() {
+    const projectTodo = document.createElement("div");
+    projectTodo.classList.add('project-todo', 'relative', 'db', 'mt2');
+
+    const todoLabel = document.createElement('label');
+    todoLabel.classList.add('todo-label', 'dib', 'mw-100', 'pl3', 'mv1');
+
+    const todoInput = document.createElement('input');
+    todoInput.classList.add('todo-input', 'absolute', 'nl3', 'mt1');
+    todoInput.type = 'checkbox';
+    if (this.completed) todoInput.checked = true;
+
+    const todoTitle = document.createElement('div');
+    todoTitle.classList.add('todo-title', 'f3');
+    todoTitle.innerText = this.title;
+
+    const todoDescription = document.createElement('span');
+    todoDescription.classList.add('todo-description', 'mv1');
+    todoDescription.innerText = this.description;
+
+    const todoInfo = document.createElement('div');
+    todoInfo.classList.add('todo-info', 'mv1');
+
+    const todoDueDate = document.createElement('span');
+    todoDueDate.classList.add('todo-due-date', 'card-subtitle');
+    todoDueDate.innerText = this.dueDate;
+
+    const infoDivider = document.createElement('span');
+    infoDivider.classList.add('info-divider');
+    infoDivider.innerText = ' | ';
+
+    const todoPriority = document.createElement('span');
+    todoPriority.classList.add('todo-priority', 'gold');
+    todoPriority.innerText = this.priority;
+
+    projectTodo.appendChild(todoLabel);
+    todoLabel.appendChild(todoInput);
+    todoLabel.appendChild(todoTitle);
+    todoLabel.appendChild(todoDescription);
+    todoLabel.appendChild(todoInfo);
+    todoInfo.appendChild(todoDueDate);
+    todoInfo.appendChild(infoDivider);
+    todoInfo.appendChild(todoPriority);
+
+    return projectTodo;
+  }
 }
+
+export { TodoItem };
