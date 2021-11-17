4ccd0daf81b0ef793d31d9e7d04eb508ce7391f7
harmolipi <nikob381@gmail.com>
2021-11-17 13:54:36 -0500

    Added blank default project

--- src/default-project-module.js
+++ src/default-project-module.js
@@ -2,9 +2,15 @@ import { Project } from './project-module';
 import { TodoItem } from './todo-item-module';
 
 const generateDefault = () => {
-  const myProject1 = new Project('Project title', 'Project description');
-  const myProject2 = new Project('Another project', 'This is a great project.');
-  const myProject3 = new Project('Project title', 'Project description');
+  const defaultProject = new Project('');
+
+  return [defaultProject];
+};
+
+const generateDevDefault = () => {
+  const myProject1 = new Project('Project title');
+  const myProject2 = new Project('Another project');
+  const myProject3 = new Project('Project title');
   const myTodo1 = new TodoItem('test title', 'test description', 'test due date', 'medium', true);
   const myTodo2 = new TodoItem('Title', 'This is the description of this todo', 'Due date', 'high', false);
   const myTodo3 = new TodoItem("Here's another task", 'This task will entail many things that need to be done.', 'Due date', 'low', true);
@@ -16,6 +22,6 @@ const generateDefault = () => {
   myProject3.addTodo(myTodo3);
 
   return [myProject1, myProject2, myProject3];
-}
+};
 
-export { generateDefault };
+export { generateDefault, generateDevDefault };
