2a41f8814c14255a6573dffae3d7f73ce814a449
harmolipi <nikob381@gmail.com>
2021-11-17 17:06:02 -0500

    Created project and todo ids

--- src/project-module.js
+++ src/project-module.js
@@ -3,13 +3,29 @@ class Project {
   id;
   todoItems;
 
-  constructor(name) {
+  constructor(name, id) {
     this.name = name;
+    this.id = id;
     this.todoItems = [];
   }
 
   addTodo(todo) {
     this.todoItems.push(todo);
+    this.updateTodoList();
+  }
+
+  updateTodoList() {
+    let tempTodos = this.todoItems.slice();
+    this.#clearTodos();
+    tempTodos.forEach((todo) => {
+      todo.project_id = this.id;
+      this.todoItems.push(todo);
+      todo.id = this.todoItems.length - 1;
+    });
+  }
+
+  #clearTodos() {
+    this.todoItems = [];
   }
 }
 
--- src/todo-item-module.js
+++ src/todo-item-module.js
@@ -4,13 +4,17 @@ class TodoItem {
   dueDate;
   priority;
   completed;
+  id;
+  project_id;
 
-  constructor(title, description, dueDate, priority, completed) {
+  constructor(title, description, dueDate, priority, completed, project_id, id) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.completed = completed;
+    this.project_id = project_id;
+    this.id = id;
   }
 }
 
