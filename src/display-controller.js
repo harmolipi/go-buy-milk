8c9f8004f3c83edf3a2e299df20689e76b634282
harmolipi <nikob381@gmail.com>
2021-11-17 12:22:13 -0500

    Added new project modal

--- src/display-controller.js
+++ src/display-controller.js
@@ -6,18 +6,24 @@ class DisplayController {
   static #projects;
   static #navbar;
   static #newProjectButton;
+  static #newProjectInput;
+  static #newProjectModal;
 
   static initialize() {
     this.#body = document.querySelector('body');
     this.#content = this.createContentContainer();
     this.#projects = this.createProjectsContainer();
     this.#navbar = this.createNavbar();
-    this.#newProjectButton = this.createNewProjectButton();
+    this.#newProjectButton = this.#createNewProjectButton();
+    this.#newProjectInput = this.#createNewProjectInput();
+    this.#newProjectModal = this.#createNewProjectModal(this.#newProjectForm());
 
     this.#body.appendChild(this.#content);
     this.#content.prepend(this.#navbar);
     this.#content.appendChild(this.#projects);
     this.#projects.prepend(this.#newProjectButton);
+    this.#projects.appendChild(this.#newProjectInput);
+    this.#projects.appendChild(this.#newProjectModal);
   }
 
   static updateDisplay(display = ProjectsController.projects) {
@@ -70,10 +76,14 @@ class DisplayController {
     return navbar;
   }
 
-  static createNewProjectButton() {
-    const newProjectButton = document.createElement('button');
-    newProjectButton.setAttribute('id', 'new-project-button');
-    newProjectButton.classList.add('article', 'mw5', 'bg-white', 'f4', 'f3-ns', 'pa2', 'pa3-ns', 'ma3', 'flex', 'flex-column', 'items-center');
+  static #createNewProjectButton() {
+    const newProjectButton = document.createElement('div');
+    newProjectButton.setAttribute('id', 'new-project-button2');
+    newProjectButton.classList.add('mw5', 'pa1', 'ma3', 'flex');
+
+    const buttonLabel = document.createElement('label');
+    buttonLabel.setAttribute('for', 'new-project-input');
+    buttonLabel.classList.add('paper-btn', 'bg-white', 'f4', 'f3-ns', 'flex', 'flex-column', 'items-center', 'pa2', 'pa3-ns');
   
     const plusSign = document.createElement('span');
     plusSign.classList.add('f1');
@@ -82,10 +92,85 @@ class DisplayController {
     const addNewProject = document.createElement('span');
     addNewProject.innerText = 'Add New Project';
     
-    newProjectButton.appendChild(plusSign);
-    newProjectButton.appendChild(addNewProject);
+    newProjectButton.appendChild(buttonLabel);
+    buttonLabel.appendChild(plusSign);
+    buttonLabel.appendChild(addNewProject);
   
     return newProjectButton;
+  }
+
+  static #createNewProjectInput() {
+    const newInput = document.createElement('input');
+    newInput.setAttribute('id', 'new-project-input');
+    newInput.setAttribute('type', 'checkbox');
+    newInput.classList.add('modal-state');
+
+    return newInput;
+  }
+
+  static #createNewProjectModal(modalBody) {
+    const newModalContainer = document.createElement('div');
+    newModalContainer.classList.add('modal');
+
+    const newModalLabel = document.createElement('label');
+    newModalLabel.setAttribute('for', 'new-project-input');
+    newModalLabel.classList.add('modal-bg');
+
+    const newModalBody = document.createElement('div');
+    newModalBody.classList.add('modal-body');
+
+    const closeLabel = document.createElement('label');
+    closeLabel.setAttribute('for', 'new-project-input');
+    closeLabel.classList.add('btn-close');
+    closeLabel.textContent = 'X';
+
+    newModalContainer.appendChild(newModalLabel);
+    newModalContainer.appendChild(newModalBody);
+    newModalBody.appendChild(closeLabel);
+    
+    if(modalBody) {
+      newModalBody.appendChild(modalBody);
+    } else {
+      const modalTitle = document.createElement('h4');
+      modalTitle.classList.add('modal-title');
+      modalTitle.textContent = 'This is the title';
+
+      const textContent = document.createElement('p');
+      textContent.classList.add('modal-text');
+      textContent.textContent = "Hello! This is a longer bit of text. It keeps going. Let's see if it wraps around.";
+
+      newModalBody.appendChild(modalTitle);
+      newModalBody.appendChild(textContent);
+    }
+
+    return newModalContainer;
+  }
+
+  static #newProjectForm() {
+    const projectContainer = document.createElement('div');
+    projectContainer.classList.add('project-container', 'ma2', 'w5', 'h5');
+
+    const projectBody = document.createElement('div');
+    projectBody.classList.add('project-body', 'overflow-scroll');
+
+    const projectTitle = document.createElement('input');
+    projectTitle.setAttribute('placeholder', 'Project Title');
+    projectTitle.setAttribute('type', 'text');
+    projectTitle.setAttribute('id', 'new-project-title');
+    projectTitle.classList.add('project-title', 'card-title');
+
+    const projectTodos = document.createElement('div');
+    projectTodos.classList.add('project-todos', 'mt-4');
+
+    projectContainer.appendChild(projectBody);
+    projectBody.appendChild(projectTitle);
+    projectBody.appendChild(projectTodos);
+
+    // for(const todo of project.todoItems) {
+    //   projectTodos.appendChild(todo.displayTodoItem());
+    // }
+
+    return projectContainer;
   }
 
   static displayProject(project) {
