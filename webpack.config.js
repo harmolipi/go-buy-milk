727f7c2e249cbee3e9df5dd4572b13258dc14b84
harmolipi <nikob381@gmail.com>
2021-11-21 18:09:38 -0500

    Dynamically create index with HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Go Buy Milk',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
